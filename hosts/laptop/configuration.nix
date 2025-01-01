a11a359e081dbba9d5b45fb38efa48d0d70bb970
Dmytro Melnyk <demetrius05@proton.me>
2025-01-01 12:50:37 +0000

    conservation mode

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -17,16 +17,9 @@ in
 		amdgpuBusId = "PCI:100:0:0";
 	};
 
-  systemd.services.enable-conservation = {
-    description = "Enable Conservation Mode";
-    after = [ "acpi.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      # Use 'bash' to interpret the script
-      ExecStart = "${pkgs.bash}/bin/bash -c 'echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode'"; 
-    };
-  };
+	# By default true
+	#options.conservationModeEnabled.enable = false;
+
   networking.hostName = hosts.workstation.hostname; 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -85,32 +78,7 @@ in
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11"; 
   system.autoUpgrade.enable = true;
   environment.variables.EDITOR = "vim";
   boot.loader = {
